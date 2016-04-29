cef4a6701d9f94ec1f2f6ad41a56f9e97290a152
JeongSanghyun <babomgu@naver.com>
2016-04-29 22:36:48 +0900

    test2

--- console.js
+++ console.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+var colors = require('colors');
+var http = require('http');
+
+http.createServer(function(req, res){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>Hello World</h1>');
+
+console.log('Hello World'.green);
+}).listen('33333', function(){
+        console.log('Server Running!!');
+});
